835c2f2e1dec2e60d5ae3a83033724ec837bcd4e
Eugen Wesseloh <codexp@gmx.net>
2020-12-19 01:32:50 +0100

    outsource filters to trait

--- src/View.php
+++ src/View.php
@@ -1,25 +1,15 @@
 <?php
 
+use View\FilterSupport;
+
 class View implements ArrayAccess
 {
+    use FilterSupport;
+
     /**
      * Global View variables
      */
     protected static array $globals = [];
-
-    /**
-     * Registered View filters
-     */
-    protected static array $filters = [
-        'esc' => 'htmlspecialchars',
-        'escape' => 'htmlspecialchars',
-        'join' => [View::class, 'filterJoin'],
-        'json' => 'json_encode',
-        'lower' => 'mb_strtolower',
-        'number' => 'number_format',
-        'price' => [View::class, 'filterPrice'],
-        'upper' => 'mb_strtoupper',
-    ];
 
     /**
      * View name
@@ -70,64 +60,9 @@ class View implements ArrayAccess
         return $this;
     }
 
-    public function filter($var, ...$filters)
-    {
-        // set default filter if nothing is specified
-        if (empty($filters)) {
-            $filters = ['escape'];
-        }
-
-        foreach ($filters as $name) {
-            // if array syntax is used
-            if (is_array($name)) {
-                // it has to be an associated array (key => value)
-                $assoc = $name;
-                // key is filter name and value is additional arguments
-                foreach ($assoc as $name => $args) {
-                    // use defined filter or fallback to global function
-                    $filter = self::$filters[$name] ?? $name;
-                    // pass var and arguments to filter
-                    $var = $filter($var, ...(array)$args);
-                }
-            } else {
-                // use defined filter or fallback to global function
-                $filter = self::$filters[$name] ?? $name;
-                // pass var to filter as the only argument
-                $var = $filter($var);
-            }
-        }
-
-        return $var;
-    }
-
     public static function setGlobals(array $globals, bool $replace = false)
     {
         self::$globals = $replace ? $globals : array_replace(self::$globals, $globals);
-    }
-
-    public static function registerFilter(string $name, callable $callable)
-    {
-        self::$filters[$name] = $callable;
-    }
-
-    public static function filterJoin(array $parts, string $glue = ', '): string
-    {
-        return implode($glue, $parts);
-    }
-
-    public static function filterPrice(
-        float $price,
-        $currency = '€',
-        $decimals = 2,
-        $decimal_separator = ',',
-        $thousands_separator = ''
-    ): string {
-        return sprintf(
-            '<span class="price%s"><span class="value">%s</span> <span class="currency">%s</span></span>',
-            $price < 0 ? ' negative' : '',
-            number_format($price, $decimals, $decimal_separator, $thousands_separator),
-            htmlentities($currency)
-        );
     }
 
     public function offsetExists($key)
