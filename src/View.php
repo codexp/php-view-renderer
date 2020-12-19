73696c2302fb8e65e2471ca0c06b416c03b0dfef
Eugen Wesseloh <codexp@gmx.net>
2020-12-19 01:46:51 +0100

    add class comment

--- src/View.php
+++ src/View.php
@@ -2,6 +2,16 @@
 
 use View\FilterSupport;
 
+/**
+ * PHP View renderer
+ *
+ * - Defines a predictable context for php view files
+ * - Buffered content rendering (returnable)
+ * - Including views inside other views
+ * - Support for view variables (inherited or restricted)
+ * - Support for global variables (available in every view, even in restricted views)
+ * - Chainable filters (can be disabled by removing FilterSupport trait)
+ */
 class View implements ArrayAccess
 {
     use FilterSupport;
